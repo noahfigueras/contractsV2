da5dd370f4cdce0e1ed9c6644e5b73fbc1d78127
noah figueras <noahfigueras@gmail.com>
2024-08-21 16:24:25 +0200

    Enable Beacon chain api calls to get block data to generate proofs

--- eip-4788-prover/prover/src/main.rs
+++ eip-4788-prover/prover/src/main.rs
@@ -1,13 +1,18 @@
 use consensus_types::{
     beacon_block::BeaconBlock,
     beacon_state::BeaconState,
+    beacon_state::BeaconBlockState,
 };
 use ssz_rs::prelude::*;
 use serde_json::{ from_str, Value };
 use std::fs;
+use reqwest::Result;
+use std::time::Duration;
+use reqwest::ClientBuilder;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    /*
     // Load from File
     let beacon_block = fs::read_to_string("data/block.json").unwrap();
     let beacon_state = fs::read_to_string("data/beacon_state@9727474.json").unwrap();
@@ -21,4 +26,29 @@ async fn main() {
     let (proof, witness) = block.prove(&["slot".into()]).unwrap();
     dbg!(&proof, &witness);
     assert!(proof.verify(witness).is_ok());
+    */
+    let timeout = Duration::new(60, 0);
+    let client = ClientBuilder::new().timeout(timeout).build()?;
+
+    let slot = 9312425;
+    let beacon_url = format!(
+        "http://unstable.mainnet.beacon-api.nimbus.team/eth/v2/beacon/blocks/{slot}",
+        slot = slot
+    );
+
+    let response = client.get(&beacon_url).send().await?;
+    if response.status().is_success() {
+        let text = response.text().await?;
+        let json: Value = from_str(text.as_str()).unwrap();
+        let block: BeaconBlock = BeaconBlock::from_json(&json["data"]["message"]);
+        let (proof, witness) = block.multi_prove(&[
+            &["proposer_index".into()],
+            &["body".into(), "execution_payload".into(), "fee_recipient".into()],
+        ]).unwrap();
+        dbg!(&proof, &witness);
+    } else {
+        dbg!("Error: API call unsuccessful");
+    }
+
+    Ok(())
 }
